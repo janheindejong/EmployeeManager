1dceef4f6ea511058a7a805b7167fe84af5b4f71
Jan Hein de Jong <janhein.dejong@gmail.com>
2023-11-29 16:18:42 +0100

    Add EmployeeViewModel

--- EmployeeManager.ViewModel/EmployeeViewModel.cs
+++ EmployeeManager.ViewModel/EmployeeViewModel.cs
@@ -1,14 +1,67 @@
-﻿using EmployeeManager.Common.Model;
+﻿using EmployeeManager.Common.DataProvider;
+using EmployeeManager.Common.Model;
 
 namespace EmployeeManager.ViewModel
 {
-    public class EmployeeViewModel
+    public class EmployeeViewModel : ViewModelBase
     {
-        private readonly Employee _employee;    
-        public EmployeeViewModel(Employee employee)
+        private readonly Employee _employee;
+        private readonly IEmployeeDataProvider _employeeDataProvider;
+
+        public EmployeeViewModel(Employee employee, IEmployeeDataProvider employeeDataProvider)
         {
             _employee = employee;
+            _employeeDataProvider = employeeDataProvider;
         }
+
+
+        public string FirstName
+        {
+            get { return _employee.FirstName; }
+            set
+            {
+                if (_employee.FirstName != value)
+                {
+                    _employee.FirstName = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged(nameof(CanSave));
+                }
+            }
+        }
+
+        public int JobRoleId
+        {
+            get { return _employee.JobRoleId; }
+            set
+            {
+                if (_employee.JobRoleId != value)
+                {
+                    _employee.JobRoleId = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public bool IsCoffeeDrinker
+        {
+            get { return _employee.IsCoffeeDrinker; }
+            set
+            {
+                if (_employee.IsCoffeeDrinker != value)
+                {
+                    _employee.IsCoffeeDrinker = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public bool CanSave() => !string.IsNullOrEmpty(FirstName);
+
+        public void Save()
+        {
+            _employeeDataProvider.SaveEmployee(_employee);
+        }
+
 
     }
 }
