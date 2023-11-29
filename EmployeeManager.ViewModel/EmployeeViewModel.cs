cc2a44183864b432c4fb5e125736756e2c31d70b
Jan Hein de Jong <janhein.dejong@gmail.com>
2023-11-29 15:38:02 +0100

    Add project files.

---
+++ EmployeeManager.ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManager.Common.Model;
+
+namespace EmployeeManager.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        private readonly Employee _employee;    
+        public EmployeeViewModel(Employee employee)
+        {
+            _employee = employee;
+        }
+
+    }
+}
