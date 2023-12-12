296205001837105bad0458056a3ef6024944d943
Jan Hein de Jong <janhein.dejong@gmail.com>
2023-12-12 14:09:28 +0100

    Minor changes

--- EmployeeManager.ViewModel/Command/DelegateCommand.cs
+++ EmployeeManager.ViewModel/Command/DelegateCommand.cs
@@ -5,11 +5,11 @@ namespace EmployeeManager.ViewModel.Command
     public class DelegateCommand : ICommand
     {
         private readonly Action _execute;
-        private readonly Func<bool> _canExecute;
+        private readonly Func<bool>? _canExecute;
 
-        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+        public DelegateCommand(Action execute, Func<bool>? canExecute = null)
         {
-            this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this._execute = execute;
             this._canExecute = canExecute;
         }
 
